c98e7557c88c6dbee973e50f98b7ea07258512f4
root <root@sd2496546.fastvps-server.com>
2026-03-30 14:17:16 +0000

    Remove old files

--- plugins/docker.zsh
+++
@@ -1,8 +0,0 @@
-# Docker aliases
-alias d='docker'
-alias dps='docker ps'
-alias di='docker images'
-alias dexec='docker exec -it'
-alias dlogs='docker logs -f'
-alias dstop='docker stop'
-alias drm='docker rm'
--- themes/custom.zsh-theme
+++
@@ -1,4 +0,0 @@
-precmd() {
-    PROMPT="%F{green}%n%f@%F{magenta}%m%f:%F{yellow}%~%f
-%F{cyan}❯%f "
-}
